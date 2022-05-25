8a167b973d8fb3325579cdb8623273f2cb93546d
lpirro <leonardo.pirro@gmail.com>
2022-05-25 14:44:42 +0200

    Formatting

--- app/src/main/java/com/lpirro/cryptomovies/data/network/interceptors/ApiKeyInterceptor.kt
+++ app/src/main/java/com/lpirro/cryptomovies/data/network/interceptors/ApiKeyInterceptor.kt
@@ -9,7 +9,9 @@ private const val API_KEY_VALUE = "api_key"
 class ApiKeyInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var original = chain.request()
-        val url = original.url.newBuilder().addQueryParameter(API_KEY_VALUE, BuildConfig.API_KEY).build()
+        val url = original.url.newBuilder()
+            .addQueryParameter(API_KEY_VALUE, BuildConfig.API_KEY)
+            .build()
         original = original.newBuilder().url(url).build()
         return chain.proceed(original)
     }
--- app/src/main/java/com/lpirro/cryptomovies/data/network/interceptors/NetworkConnectionInterceptor.kt
+++ app/src/main/java/com/lpirro/cryptomovies/data/network/interceptors/NetworkConnectionInterceptor.kt
@@ -7,7 +7,7 @@ import okhttp3.Request
 import okhttp3.Response
 import java.io.IOException
 
-class NetworkConnectionInterceptor(private val mContext: Context) : Interceptor {
+class NetworkConnectionInterceptor(private val context: Context) : Interceptor {
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         if (!isConnected) {
@@ -20,7 +20,7 @@ class NetworkConnectionInterceptor(private val mContext: Context) : Interceptor 
     private val isConnected: Boolean
         get() {
             val connectivityManager =
-                mContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             val netInfo = connectivityManager.activeNetworkInfo
             return netInfo != null && netInfo.isConnected
         }
